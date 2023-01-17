8f6c71497f36d6cc94f849f932b4e555ed99048e
Abel <abels2000@gmail.com>
2023-01-17 13:35:00 +0530

    build

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ test_requirements = [
 
 # Find first gcc directory
 def find_homebrew_gcc():
-    return glob.glob('/usr/local/Cellar/gcc*')
+    return glob.glob('/usr/local/Cellar/gcc*')[0]
 
 
 # Platform specific settings
@@ -137,6 +137,8 @@ if operating_system == 'Darwin':
                 if not ('i386' in root):
                     lib_path_list.append(root)
 
+    print(lib_path_list)
+    
     os.environ['FFLAGS'] += ' -mmacosx-version-min=10.7'
     os.environ['CFLAGS'] += ' -mmacosx-version-min=10.7'
     default_link_args = []
